70852a8909a1452df3637abbc94b8d209f421fed
IbrahimSanaullah <23100204@lums.edu.pk>
2023-04-26 15:06:38 +0500

    fix errors

---
+++ DatasetGeneration/generateTrainingData.py
@@ -0,0 +1,58 @@
+import os
+import json
+import sys
+
+fileName = sys.argv[1]
+directory = f"./graphs/{sys.argv[1]}"
+executableName = sys.argv[2]
+data = []
+
+
+with open(directory) as line:
+    graph = json.load(line)
+
+edges = list()
+hashnames = dict()
+process = dict()
+
+for i in graph:
+    if "id" in i:
+        if i['type'] == "Artifact":
+            hashnames[i['id']] = i['annotations']['path']
+            process[i['id']] = 0
+        if i['type'] == "Process":
+            hashnames[i['id']] = i['annotations']['exe']
+            process[i['id']] = 1
+    else:
+        edges.append((i['to'],i['from']))
+
+
+json_dict = {
+    "name": fileName,
+    "exe": executableName,
+    "edges": edges,
+    "hash": hashnames,
+    "types": process 
+}
+
+
+try:
+    with open('dataset.json', 'r') as f:
+        output_list = json.load(f)
+except json.decoder.JSONDecodeError:
+    # If the file is empty or not a valid JSON object, create a new empty array
+    output_list = []
+
+   
+output_list.append(json_dict)   
+
+
+with open("dataset.json", "w") as f:
+    f.write("[\n")
+    f.write(json.dumps(output_list[0]))
+    if len(output_list) > 1:
+        for obj in output_list[1:]:
+            f.write(",\n" + json.dumps(obj))
+    f.write("\n]")
+
+
