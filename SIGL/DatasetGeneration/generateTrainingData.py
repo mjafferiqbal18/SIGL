f8a0d6ca35bbcbd3cb9bb88a789f13bf18be3755
mjafferiqbal18 <mjafferiqbal18@gmail.com>
2024-04-14 21:29:41 +0500

    new dns bind9stuff dump

--- SIGL/DatasetGeneration/generateTrainingData.py
+++ SIGL/DatasetGeneration/generateTrainingData.py
@@ -1,7 +1,7 @@
 import os
 import json
 import sys
-
+import uuid
 
 def processSPADEJSON(fileName,directory):
 
@@ -19,10 +19,10 @@ def processSPADEJSON(fileName,directory):
                 try:
                     hashNames[node['id']] = node['annotations']['path']
                     process[node['id']] = 0
-                except:
+                except: 
                     hashNames[node['id']] = node['annotations']['subtype'].replace(" ", "")
                     process[node['id']] = 0
-            if node['type'] == "Process":
+            if node['type'] == "Process":  
                 hashNames[node['id']] = node['annotations']['exe']
                 process[node['id']] = 1
         # Handling edges
@@ -39,9 +39,101 @@ def processSPADEJSON(fileName,directory):
 
     return jsonDict
 
+def generate_uuid():
+    generated_uuid = uuid.uuid4()# Generate a UUID4 (random) UUID
+    uuid_hex = generated_uuid.hex #make a hex without dashes
+    return uuid_hex
 
+def processDNS(dnsFileDirectory):
+  """
+  Parses a DNS log file and returns a dictionary containing domains as keys and lists of resolved IPs as values.
 
+  Args:
+      filename: The path to the DNS log file.
 
+  Returns:
+      A dictionary containing domains as keys and lists of resolved IPs as values.
+  """
+  results = {}
+  with open(dnsFileDirectory, 'r') as f:
+    for line in f:
+      parts = line.strip().split()
+      domain = parts[0]
+      if len(parts) == 1:
+        # Domain name with no resolved IP yet
+        pass
+        # if domain not in results:
+        #   results[domain] = []
+      elif len(parts) > 1:
+        # Domain name with resolved IP(s)
+        ips = parts[1].split(',')  # Split multiple IPs if present
+        if domain not in results:
+          results[domain] = ips
+        else:
+          results[domain].extend(ips)  # Add IPs to existing list for the domain
+
+    for k,v in results.items():
+        results[k] = list(set(v))
+  return results
+
+def processSPADEJSONwithIPandDNS(fileName,directory,dnsFileDirectory):
+
+    with open(directory) as line:
+        graph = json.load(line)
+
+    edges = [] # Edges in the graph represented as a list of tuples
+    hashNames = {} # Maps hash of each node to it's path name
+    process = {} # Stores if a node is a process or artifact
+    sockets = {} # key:ip, value: list of ids
+    
+    for node in graph:
+        # Handling nodes
+        if "id" in node:
+            if node['type'] == "Artifact":
+                try:
+                    hashNames[node['id']] = node['annotations']['path']
+                    process[node['id']] = 0
+                except: 
+                    if node['annotations']['subtype']=='network socket':
+                        try:
+                            hashNames[node['id']] = 'networksocket_'+node['annotations']['remote address'] #adds ip as networksocket_192.168.33.13
+                            if sockets.get(node['annotations']['remote address']):
+                                sockets[node['annotations']['remote address']]=[node['id']]
+                            else:
+                                sockets[node['annotations']['remote address']].append(node['id'])
+                        except: 
+                            pass
+                    else:
+                        hashNames[node['id']] = node['annotations']['subtype'].replace(" ", "")
+                    process[node['id']] = 0
+            if node['type'] == "Process":  
+                hashNames[node['id']] = node['annotations']['exe']
+                process[node['id']] = 1
+        # Handling edges
+        else:
+            if node["to"] != node["from"]:  #Remove self edge
+                edges.append((node['to'],node['from']))
+
+    # resolutions: {queried domain: [resolvedIPs]}
+    resolutions=processDNS(dnsFileDirectory)
+    for d,ips in resolutions.items():
+        hash = generate_uuid()
+        hashNames[hash] = d #hash -> queried domain
+        process[hash] = 0
+        
+        for ip in ips: #all ips returned by the resolution
+            if sockets.get(ip): #if a socket was opened on that ip
+                for id in sockets[ip]:
+                    edges.append((hash,id)) #edge between domain and socket
+
+    jsonDict = {
+        "name": fileName,
+        "edges": edges,
+        "hash": hashNames,
+        "types": process
+    }
+
+    return jsonDict
 
 def generateDataset():
 
@@ -71,3 +163,48 @@ def generateDataset():
             for obj in outputList[1:]:
                 f.write(",\n" + json.dumps(obj))
         f.write("\n]")
+
+def generateDatasetwithDNS():
+    base = os.path.dirname(os.path.abspath(__file__))
+    trainingDir = os.path.join(base, "trainingGraphsWithDNS")
+    files = os.listdir(trainingDir)
+    graphsList=[]
+    dnsList=[]
+
+    for f in files:
+        if f.endswith('.txt'):
+            dnsList.append(f)
+        else:
+            graphsList.append(f)
+
+    outputList = []
+
+    for graph in graphsList:
+        graphName= graph.split('.') #gets you 'graph-1' for example from 'graph-1.json'
+        graphDir = os.path.join(trainingDir, graph)
+        dnsLogName= graphName[0]+'txt'
+        dnsLogDir=None
+
+        if dnsLogName in dnsList:
+            dnsLogDir=os.path.join(trainingDir, dnsLogName)
+        
+        if dnsLogDir:
+            jsonDict = processSPADEJSONwithIPandDNS(graphName[0].replace('-',''),graphDir,dnsLogDir)
+            outputList.append(jsonDict)
+        else:
+            jsonDict = processSPADEJSON(graphName[0].replace('-',''),graphDir)
+            outputList.append(jsonDict)
+
+    with open(os.path.join(base, "trainingDataset.json") , "w") as f:
+        f.write("[\n")
+        f.write(json.dumps(outputList[0]))
+        if len(outputList) > 1:
+            for obj in outputList[1:]:
+                f.write(",\n" + json.dumps(obj))
+        f.write("\n]")
+
+def main():
+    generateDatasetwithDNS()
+
+if __name__ == "__main__":
+    main()
---
+++ SIGgenerator/test-spade-script_new.sh
@@ -0,0 +1,420 @@
+#!/bin/bash
+
+debug=0
+spade_bin="./bin/spade"
+spade_cfg="./cfg/spade.client.Control.config"
+
+cmd_bin_logs_dir_path="/home/vagrant/trace_bins"
+spade_log_msg_str_that_signals_the_cdm_log_has_been_processed="Finished reading"
+dot_output_dir_path="/home/vagrant/trace_dots"
+
+
+clear_spade_cfg(){
+  truncate -s 0 "${spade_cfg}"
+}
+
+clear_postgresql_storage(){
+  ./bin/manage-postgres.sh clear
+}
+
+
+is_spade_running(){
+  "${spade_bin}" status | grep -q "Running"
+}
+
+
+stop_spade(){
+  "${spade_bin}" stop
+}
+
+
+kill_spade(){
+  "${spade_bin}" kill
+}
+
+try_stop_kill_spade(){
+  #if is_spade_running; then
+   # stop_spade
+  ##fi
+  #sleep 5
+  if is_spade_running; then
+    kill_spade
+  fi
+}
+
+
+start_spade(){
+  "${spade_bin}" start
+}
+
+send_spade_command(){
+  local cmd="${1}"
+  echo "${cmd}" | "${spade_bin}" control
+  if [ "${debug}" -eq 1 ]; then
+  echo "list all" | "${spade_bin}" control
+  fi
+}
+
+
+install_skype(){
+  echo "Installing skype..."
+  #wget https://go.skype.com/skypeforlinux-64.deb
+  sudo apt install ./skypeforlinux-64.deb -y
+
+  if [ $? -eq 0 ]; then
+   echo "Installation of skype was successful."
+  else
+   echo "Installation of skype for Linux failed."
+   exit 1
+  fi
+
+  #sudo rm skypeforlinux-64.deb
+}
+
+remove_skype(){
+  echo "Removing skype..."
+  sudo apt remove skypeforlinux -y
+  sudo apt autoremove -y
+  echo "Skype has been removed"
+}
+
+
+install_filezilla(){
+  echo "Installing filezilla..."
+  sudo apt install filezilla -y
+
+  if [ $? -eq 0 ]; then
+   echo "Installation of Teamviewer for Linux was successful."
+  else
+   echo "Installation of Teamviewer for Linux failed."
+   exit 1
+  fi
+
+}
+
+remove_filezilla(){
+  echo "Removing filezilla..."
+  sudo apt remove filezilla -y
+  echo "Filezilla has been removed"
+}
+
+install_teamviewer(){
+  echo "Installing teamviewer..."
+#  wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+  sudo apt install ./teamviewer_amd64.deb -y
+
+  if [ $? -eq 0 ]; then
+   echo "Installation of Teamviewer for Linux was successful."
+  else
+   echo "Installation of Teamviewer for Linux failed."
+   exit 1
+  fi
+
+  #sudo rm teamviewer_amd64.deb
+}
+
+remove_teamviewer(){
+  echo "Remove teamviewer..."
+  sudo apt remove teamviewer -y
+  sudo apt autoremove -y
+  echo "Teamviewer has been removed"
+}
+
+install_pwsafe(){
+  echo "Installing pwsafe..."
+  sudo apt install passwordsafe -y
+  if [ $? -eq 0 ]; then
+    echo "Installation of pwsafe for Linux was successful."
+  else
+    echo "Installation of pwsafe for Linux failed."
+    exit 1
+  fi
+}
+
+remove_pwsafe(){
+  echo "Removing pwsafe..."
+  sudo apt remove passwordsafe -y
+  sudo apt autoremove -y
+  echo "Pwsafe has been removed"
+}
+
+install_7zip(){
+  echo "Installing 7zip..."
+  sudo apt install p7zip-full p7zip-rar -y
+  if [ $? -eq 0 ]; then
+    echo "Installation of 7zip for Linux was successful."
+  else
+    echo "Installation of 7zip for Linux failed."
+    exit 1
+  fi
+}
+
+remove_7zip(){
+  echo "Removing 7zip..."
+  sudo apt remove p7zip -y
+  sudo apt autoremove -y
+  echo "7zip has been removed"
+}
+
+install_dropbox(){
+  echo "Installing dropbox..."
+  #wget -O dropbox.deb https://linux.dropbox.com/packages/ubuntu/dropbox_2019.02.14_amd64.deb
+  sudo apt install ./dropbox.deb -y
+  if [ $? -eq 0 ]; then
+    echo "Installation of dropbox for Linux was successful."
+  else
+    echo "Installation of dropbox for Linux failed."
+    exit 1
+  fi
+  #sudo rm ./dropbox.deb
+}
+
+remove_dropbox(){
+  echo "Removing dropbox..."
+  sudo apt remove dropbox -y
+  sudo apt autoremove -y
+  echo "Dropbox has been removed"
+}
+
+
+install_firefox(){
+  echo "Installing firefox..."
+  sudo apt install firefox -y
+  if [ $? -eq 0 ]; then
+   echo "Installation of firefox for Linux was successful."
+  else
+   echo "Installation of firefox for Linux failed."
+   exit 1
+  fi
+}
+
+remove_firefox(){
+  echo "Removing firefox..."
+  sudo apt purge firefox -y
+  sudo apt remove firefox
+  sudo apt autoremove -y
+  echo "Firefox has been removed"
+}
+
+
+install_qbittorrent(){
+  echo "Installing qbittorrent..."
+  sudo apt install qbittorrent -y
+  if [ $? -eq 0 ]; then
+   echo "Installation of qbittorrent for Linux was successful."
+  else
+   echo "Installation of qbittorrent for Linux failed."
+   exit 1
+  fi
+}
+
+remove_qbittorrent(){
+  echo "Removing qbittorrent..."
+  sudo apt purge qbittorrent -y
+  sudo apt remove qbittorrent
+  sudo apt autoremove -y
+  echo "qbittorrent has been removed"
+}
+
+install_geany(){
+  echo "Installing geany..."
+  sudo apt install geany -y
+  if [ $? -eq 0 ]; then
+   echo "Installation of geany for Linux was successful."
+  else
+   echo "Installation of geany for Linux failed."
+   exit 1
+  fi
+}
+
+remove_geany(){
+  echo "Removing geany..."
+  sudo apt purge geany -y
+  sudo apt remove geany
+  sudo apt autoremove -y
+  echo "geany has been removed"
+}
+
+install_gimp(){
+  echo "Installing gimp..."
+  sudo apt install gimp -y
+  if [ $? -eq 0 ]; then
+   echo "Installation of gimp for Linux was successful."
+  else
+   echo "Installation of gimp for Linux failed."
+   exit 1
+  fi
+}
+
+remove_gimp(){
+  echo "Removing gimp..."
+  sudo apt purge gimp -y
+  sudo apt remove gimp
+  sudo apt autoremove -y
+  echo "gimp has been removed"
+}
+
+
+display_help() {
+  echo "Usage: ./test-spade-script.sh [Number of Logs] [Software] [Exec path]"
+  echo "This script collects logs using SPADE. Make sure to setup SPADE first."
+  echo
+  echo "OPTIONS:"
+  echo "  -h, --help          Display this help message and exit."
+  echo "  -l                  List the available softwares."
+  echo
+  echo "Arguments:"
+  echo "  Number of logs      Specify the number of logs you want to collect."
+  echo "  Software            Specify the software for which you want to collect the log."
+  echo "  Exec path           Specify the executable path of the software."
+  echo
+  echo "Examples:"
+  echo "  ./test-spade-script.sh 10 onedrive /usr/bin/onedrive"
+}
+
+is_number() {
+  #val=$(($1))
+  if [[ $1 =~ ^[0-9]+$ ]]; then
+    echo true
+  else
+    echo "Argument '$1' is not a number."
+    echo false
+  fi
+}
+
+
+parse_commands(){
+  total_arguments=$#
+  if [[ $total_arguments == 1 && $1 == "-h" || $1 == "--help" ]]; then
+    display_help
+  elif [[ $total_arguments == 3 && $(is_number "$1") == "true" && $2 = @(firefox|Firefox|Gimp|gimp|geany|Geany|skype|Skype|teamviewer|Teamviewer|filezilla|Filezilla|dropbox|Dropbox|"7zip"|pwsafe|Pwsafe|qbittorrent|Qbittorrent) ]]; then
+    main_function "$1" "$2" "$3"
+  elif [[ $total_arguments == 1 && $1 == "-l" ]]; then
+    echo -e "Available Softwares:Skype\nTeamviewer\nFilezilla\nDropbox\n7zip\nGeany\nPwsafe\nQbittorrent\nFirefox\nGimp"
+  else
+    # Unrecognized option or argument
+    echo "Error: Incorrect arguments passed"
+    echo "Use './test-spade-script.sh --help' to display the help message."
+    exit 1
+  fi
+}
+
+# Start packet capture
+start_capture() {
+    tshark -i any -f "port 53" -w /home/vagrant/sigl_dns_logs/dns_capture.pcap &
+    # Save the process ID of tshark to a file for later use
+    echo $! > /tmp/tshark_pid
+    echo "Packet capture started..."
+}
+
+
+# Stop packet capture
+stop_capture() {
+    # Check if the process ID file exists
+    if [ -f "/tmp/tshark_pid" ]; then
+        # Read the process ID from the file
+        tshark_pid=$(cat /tmp/tshark_pid)
+        # Terminate the tshark process
+        sudo kill $tshark_pid
+        echo "Packet capture stopped."
+        # Remove the process ID file
+        rm /tmp/tshark_pid
+    else
+        echo "Packet capture process not found."
+    fi
+}
+
+main_function(){
+  local itr=$1
+  local name=$2
+  local exec_path=$3
+
+  dir_path="/home/vagrant/sigl_dns_logs/"
+
+  # Check if the directory exists
+  if [ ! -d "$dir_path" ]; then
+    # Directory does not exist, create it
+    mkdir -p "$dir_path"
+    echo "Directory created: $dir_path"
+  else
+    echo "Directory already exists: $dir_path"
+  fi
+
+  auditctl -b 1000000
+
+  for ((i=1; i<=$itr; i++))
+  do
+    #cd /home/vagrant/SPADE
+    #echo "Clearing SPADE config"
+    #clear_spade_cfg
+    #sleep 2
+    #echo "Clearing SPADE manage-postgres"
+    #clear_postgresql_storage
+    #sleep 2
+    #start_spade
+    #echo "Running SPADE Control"
+    #send_spade_command "add reporter Audit localEndpoints=true namespaces=true IPC=true networkAddressTranslation=true outputLog=/tmp/$name-$i.log"
+    #send_spade_command "exit"
+    cd /home/vagrant/
+    echo "Starting DNS Capture"
+    start_capture # DNS Capture
+    sleep 5
+    if [[ $name = @(geany|Geany) ]]; then
+      install_geany
+    elif [[ $name = @(Gimp|gimp) ]]; then
+      install_gimp
+    elif [[ $name = @(teamviewer|Teamviewer) ]]; then
+      install_teamviewer
+    elif [[ $name = @(skype|Skype) ]]; then
+      install_skype
+    elif [[ $name = @(filezilla|Filezilla) ]]; then
+      install_filezilla
+    elif [[ $name = @(dropbox|Dropbox) ]]; then
+      install_dropbox
+    elif [[ $name = @(7zip) ]]; then
+      install_7zip
+    elif [[ $name = @(qbittorrent|Qbittorrent) ]]; then
+      install_qbittorrent
+    elif [[ $name = @(pwsafe|Pwsafe) ]]; then
+      install_pwsafe
+    elif [[ $name = @(firefox|Firefox) ]]; then
+      install_firefox
+    fi
+
+    sleep 2
+    #cd /home/vagrant/SPADE
+    #send_spade_command "remove reporter Audit"
+    #stop_spade
+    stop_capture
+    sleep 5
+    # Remove software installation
+    if [[ $name = @(geany|Geany) ]]; then
+      remove_geany
+    elif [[ $name = @(Gimp|gimp) ]]; then
+      remove_gimp
+    elif [[ $name = @(teamviewer|Teamviewer) ]]; then
+      remove_teamviewer
+    elif [[ $name = @(skype|Skype) ]]; then
+      remove_skype
+    elif [[ $name = @(filezilla|Filezilla) ]]; then
+      remove_filezilla
+    elif [[ $name = @(dropbox|Dropbox) ]]; then
+      remove_dropbox
+    elif [[ $name = @(7zip) ]]; then
+      remove_7zip
+    elif [[ $name = @(qbittorrent|Qbittorrent) ]]; then
+      remove_qbittorrent
+    elif [[ $name = @(pwsafe|Pwsafe) ]]; then
+      remove_pwsafe
+    elif [[ $name = @(firefox|Firefox) ]]; then
+      remove_firefox
+    fi
+
+    #mv /tmp/$name-$i.log  /home/vagrant/sigl_logs/
+  done
+  #source ~/SIGgenerator/spade-query-script.sh $itr $name $exec_path
+
+}
+
+#main_function
+parse_commands $@
